a1940036e0af03250036f66598d35d27c94ddb76
John <potekhine@gmail.com>
2018-08-24 18:25:54 +0300

    add 2 and 3 js

--- 3_lesson/js/1.js
+++ 3_lesson/js/1.js
@@ -1,2 +1,17 @@
 'use strict';
 
+let number = 0;
+let word;
+
+do {
+    if (number === 0) {
+        word = '- это ноль';
+    } else if (number % 2 !== 0) {
+        word = '- это нечетное число';
+    } else {
+        word = '- это четное число';
+    }
+
+    console.log(number, word);
+    number++;
+} while (number <= 10);
